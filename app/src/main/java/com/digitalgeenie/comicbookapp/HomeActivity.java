3871675b3aa727e016300cf67e7afe72cbd1f41d
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-01-09 10:06:53 +0530

    Fixes issue #17 - Implement Home Page

--- app/src/main/java/com/digitalgeenie/comicbookapp/HomeActivity.java
+++ app/src/main/java/com/digitalgeenie/comicbookapp/HomeActivity.java
@@ -1,23 +1,34 @@
 package com.digitalgeenie.comicbookapp;
 
 import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.widget.ProgressBar;
+import android.util.TypedValue;
+import android.view.View;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.digitalgeenie.comicbookapp.adapters.HomeAdapter;
+import com.digitalgeenie.comicbookapp.models.HomeModel;
+import com.digitalgeenie.comicbookapp.utils.ToastUtil;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static com.digitalgeenie.comicbookapp.utils.navigationdrawer.NavigationDrawerUtil.setupDrawerContent;
+import static com.digitalgeenie.comicbookapp.utils.network.Connectivity.isConnected;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private DatabaseReference databaseReference;
-    private ProgressBar progressBar;
+    private List<HomeModel> itemList;
     private DrawerLayout mDrawer;
     private Toolbar toolbar;
     private ActionBarDrawerToggle drawerToggle;
@@ -27,7 +38,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        progressBar = findViewById(R.id.progressBar);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -38,7 +49,79 @@ public class HomeActivity extends AppCompatActivity {
         NavigationView nvDrawer = findViewById(R.id.nvView);
         setupDrawerContent(mDrawer, this, nvDrawer);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference();
+        if (isConnected(this)) {
+            itemList = new LinkedList<>();
+            prepareView();
+        } else {
+            ToastUtil.longToast("Sorry! Please check your internet connection", this);
+        }
+
+        HomeAdapter homeAdapter = new HomeAdapter(this, itemList);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(homeAdapter);
+    }
+
+    private void prepareView() {
+
+        HomeModel homeModel1 = new HomeModel("Trivia", R.drawable.trivia);
+        HomeModel homeModel2 = new HomeModel("Comics", R.drawable.comics);
+        HomeModel homeModel3 = new HomeModel("New Arrivals", R.drawable.new_arrivals);
+        HomeModel homeModel4 = new HomeModel("Store Locator", R.drawable.store_locator);
+
+        itemList.add(homeModel1);
+        itemList.add(homeModel2);
+        itemList.add(homeModel3);
+        itemList.add(homeModel4);
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx() {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, r.getDisplayMetrics()));
+    }
+
+    /**
+     * RecyclerView item decoration - give equal margin around grid item
+     */
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
     }
 
     private ActionBarDrawerToggle setupDrawerToggle() {
